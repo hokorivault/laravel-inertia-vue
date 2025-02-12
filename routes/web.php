e9544c20a66ebb51baab234fa03199208fd21e20
hokorivault <gomibako1199@gmail.com>
2025-02-12 14:34:36 +0800

    Add Routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    // return view('welcome');
     return Inertia::render('Home');
 });
+
+// inertia() way
+Route::get('/spage', function() {
+    return inertia('SpecialPage', ['vtuber' => 'Suiyo']);
+});
+
+// Shorthand
+Route::inertia('/about', 'About', ['user' => 'Yutorin']);
